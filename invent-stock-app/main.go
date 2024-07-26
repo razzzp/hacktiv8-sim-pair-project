e06a147d9b6b482cc61040d6e56cd7d9dad12087
Naufal <naufalyunan@Naufals-MacBook-Pro.local>
2024-07-26 23:41:24 +0700

    init env and db connection

--- invent-stock-app/main.go
+++ invent-stock-app/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Semangat...")
+	//load .env file
+	err := godotenv.Load("../.env")
+	if err != nil {
+	  log.Fatal("Error loading .env file ", err)
+	}
+
+	//db connection
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", 
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	db, err := sql.Open("mysql", dns)
+	if err != nil {
+		log.Fatal("Error connecting to database ", err)
+	}
+	defer db.Close()
+
+	fmt.Println("Connection to db successful ", db)
 }
